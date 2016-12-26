ba5054ede7fd60ee6843952ca5151b71cc0e262a
wangdi <wangdicoder@gmail.com>
2016-12-26 23:44:56 +1100

    add msvc12.0

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -27,10 +27,19 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-		glview = GLViewImpl::createWithFullScreen("ADisplay");
-		//glview = GLViewImpl::createWithRect("ADisplay", Rect(0, 0, 1245, 700));
+
+		int monitorCount;
+		auto monitors = glfwGetMonitors(&monitorCount);
+		if (nullptr == monitors)
+			return false;
+
+		const GLFWvidmode* videoMode = glfwGetVideoMode(monitors[1]);
+
+		glview = GLViewImpl::createWithFullScreen("ADisplay", videoMode, monitors[1]);
+		//glview = GLViewImpl::createWithRect("ADisplay", Rect(30, 500, 1245, 700));
         director->setOpenGLView(glview);
     }
+
 	glview->setDesignResolutionSize(1366, 768, ResolutionPolicy::FIXED_HEIGHT);
 
 	FileUtils::getInstance()->addSearchPath("images");
